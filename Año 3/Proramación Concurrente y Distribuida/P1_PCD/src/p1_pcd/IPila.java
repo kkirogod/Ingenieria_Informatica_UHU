0bb245fb6ca4fadf355f9d743faa225606309ffa
Miguel Quiroga <96502043+kkirogod@users.noreply.github.com>
2024-01-21 13:25:26 +0100

    Add files via upload

---
+++ Año 3/Proramación Concurrente y Distribuida/P1_PCD/src/p1_pcd/IPila.java
@@ -0,0 +1,12 @@
+package p1_pcd;
+
+public interface IPila {
+
+    public int GetNum();
+
+    public void Apila(Object elemento) throws Exception;
+
+    public Object Desapila() throws Exception;
+
+    public Object Primero() throws Exception;
+}
